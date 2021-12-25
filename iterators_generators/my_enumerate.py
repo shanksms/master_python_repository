2e71c252c9ed7bf894f272b59e70a9c100e72a29
shanksms <sriv.srivastava@gmail.com>
2021-12-25 10:41:53 +0530

    enumerate generator

--- iterators_generators/my_enumerate.py
+++ iterators_generators/my_enumerate.py
@@ -43,9 +43,28 @@ class MyIterator():
         return value
 
 
+class MyEnumerateGenerator():
+    def __init__(self, data, start_index=0):
+        self.data = data
+        self.index = 0
+        self.start_index = start_index
+
+    def __iter__(self):
+        while self.index < len(self.data):
+            value = self.start_index, self.data[self.index]
+            self.index += 1
+            self.start_index += 1
+            yield value
+
+
+
+
 if __name__ == '__main__':
     for index, letter in MyEnumerate('abc', 1):
         print(f'{index} : {letter}')
     print('#' * 60)
     for index, letter in MyEnumerateWithDelegatedIterator('abc', 2):
+        print(f'{index} : {letter}')
+    print('#' * 60)
+    for index, letter in MyEnumerateGenerator('abc', 2):
         print(f'{index} : {letter}')
